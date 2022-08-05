1848445724b37c18bf45201b35a6c9f5facf5247
Alexandre Lima <48870020+AleeZL@users.noreply.github.com>
2022-08-05 20:40:51 -0300

    Adição de imagem de exemplo

--- deadlines/app/Main.hs
+++ deadlines/app/Main.hs
@@ -3,7 +3,7 @@
 
 module Main where
 
-import qualified GI.Gtk as Gtk
+import GI.Gtk as Gtk
 
 main :: IO ()
 main = do
@@ -20,6 +20,30 @@ main = do
     Gtk.onWidgetDestroy window Gtk.mainQuit
 
 
+
+    image1 <- Gtk.imageNewFromFile $ "./images/discordqr.png"
+
+    btn1 <- Gtk.buttonNew
+    Gtk.buttonSetImage btn1  $ Just image1
+    Gtk.buttonSetRelief btn1 Gtk.ReliefStyleNone
+    Gtk.widgetSetHexpand btn1 False --evita que haja expansão vertical do botão (height)
+    Gtk.on btn1 #clicked $ do
+                        putStrLn "Teste!"
+                        Gtk.widgetDestroy window
+
+
+    grid <- Gtk.gridNew
+    Gtk.gridSetColumnSpacing grid 10
+    Gtk.gridSetRowSpacing grid 10
+    Gtk.gridSetColumnHomogeneous grid True
+                    --os primeiros 2 valores são as coordenadas x e y.
+                    --os segundos 2 valores são quantas linhas e colunas o widget deve ocupar.
+    #attach grid btn1 0 0 1 1
+
+    #add window grid
+
+
     #showAll window
     Gtk.main
 
+-- attacTo :: 
