3310994a13e1dcf7c40c2fea5dfe3168048cb7dd
tiago.simionato <tiago.simionato5@gmail.com>
2022-08-14 17:40:56 -0300

    Funções de filtro

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,15 +3,7 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
-module Lib
-  (Prefs,
-  Category,
-  Event,
-  mkEvent,
-  encodeEventsToFile,
-  decodeEventsFromFile,
-  insertToFile
-  ) where
+module Lib where
 
 import Control.Exception (IOException)
 import qualified Control.Exception as Exception
@@ -22,7 +14,7 @@ import qualified Data.ByteString.Lazy as BL
 
 import Data.Csv as Cassava
 
-import Data.Text (Text)
+import Data.Text (Text, isInfixOf)
 import qualified Data.Text.Encoding as Text
 
 import Graphics.UI.Gtk ( Color(..) )
@@ -41,13 +33,20 @@ instance ToField adtName
 
 Para o cassava saber como transformar esse ADT em uma parte do registro csv e vice versa
 -}
+
+categoryPath :: FilePath
+categoryPath = "./csv/categories.csv"
+
+eventPath :: FilePath
+eventPath = "./csv/events.csv"
+
 data Prefs =
   Prefs
     { autodelete :: Bool
     }
 
 data Category =
-  Cat
+  Category
     { catName :: Text
     , catColor :: Text
     }
@@ -66,7 +65,19 @@ data Event =
   deriving (Eq, Show)
 
 -- ADT que indica a fonte de onde o evento recebe sua cor. O usuário poderá optar por dar uma cor especifca para um evento apenas, ele poderá deixar o evento com a mesma cor da categoria que ele pertence, ou deixa para a cor ser ajustada automaticamente conforme a data se aproxima.
-data ColorSrc = Custom Text | Category | Gradient deriving (Eq, Show)
+data ColorSrc = Custom Text | CatName | Gradient deriving (Eq, Show)
+
+instance FromNamedRecord Prefs where
+  parseNamedRecord m =
+    Prefs
+      <$> m .: "autodelete"
+
+-- Para o Cassava decodificar o campo do csv e transformar no meu ADT, ele tem que ser instancia de FromNamedRecord
+instance FromNamedRecord Category where
+  parseNamedRecord m =
+    Category
+      <$> m .: "catname"
+      <*> m .: "catcolor"
 
 -- Para o Cassava decodificar os registros, eles tem que ser instância de FromRecord (sem Header) ou FromNamedRecord (com Header)
 instance FromNamedRecord Event where
@@ -83,20 +94,22 @@ instance FromNamedRecord Event where
 
 -- Usado para o Cassava saber como ler o que vem do CSV e transformar em booleano
 instance FromField Bool where
-  --parseField "True" =
-  --  pure True
+  parseField "True" =  pure True
+  parseField _      = pure False
 
-  --parseField otherType =
-  --  pure False
-  parseField field
-    | field == "True" = pure True
-    | otherwise       = pure False
-
+-- Usado para o Cassava saber como ler o que esta escrito no CSV e transformar no meu ADT de Cor. Se a string que estiver nesse campo não for 'Category' nem 'Gradient, ela sera um código hexadecimal, que sera transferido para minha cor Custom.
 instance FromField ColorSrc where
-  parseField "Category" = pure Category
+  parseField "Category" = pure CatName
   parseField "Gradient" = pure Gradient
   parseField othertype = Custom <$> parseField othertype
---TODO:  parseField color = Custom color
+
+instance ToNamedRecord Category where
+  toNamedRecord Category{..} =
+    Cassava.namedRecord
+    -- noCSV .= noADT
+    [ "catname"  .= catName
+    , "catcolor" .= catColor
+    ]
 
 -- Para o Cassava codificar o ADT para csv, é necessário que o ADT seja uma instancia de ToNamedRecord (ToRecord sem Header). Se o ADT tiver um outro ADT dentro dele, tbm é necessário 
 instance ToNamedRecord Event where
@@ -119,7 +132,7 @@ instance ToField Bool where
 
 instance ToField ColorSrc where
   toField Gradient      = "Gradient"
-  toField Category      = "Category"
+  toField CatName       = "Category"
   toField (Custom text) = toField text
 
 -- Diz para o Cassava a ordem padrão do meu Header
@@ -136,9 +149,16 @@ instance DefaultOrdered Event where
       , "color"
       ]
 
+-- Tem que por aqui o que estiver escrito no CSV, é a primeira linha do arquivo
+categoryHeader :: Header
+categoryHeader = Vector.fromList ["catname", "catcolor"]
+
 -- Tem que por aqui o que estiver escrito no CSV
 eventHeader :: Header
 eventHeader = Vector.fromList ["name", "day", "month", "year", "description", "category", "recurrent", "color"]
+
+mkCategory :: Text -> Text -> Category
+mkCategory n c = Category {catName = n, catColor = c}
 
 mkEvent :: Text -> Int -> Int -> Int -> Text -> Text -> Bool -> ColorSrc -> Event
 mkEvent n d m y desc cat re c = Item {name = n, day = d, month = m, year = y,  description = desc, category = cat, recurrent = re, color = c}
@@ -149,10 +169,10 @@ encodeEvent = Cassava.encodeByName eventHeader
 
 -- Dado que Event é uma instancia de DefaultOrdered, posso codificar assim tambem. Eh util receber um vector event pq é isso que o cassava retorna
 -- Foldable.toList transforma o Vector em []
-encodeEvent' :: Vector Event -> ByteString
-encodeEvent' = Cassava.encodeDefaultOrderedByName . Foldable.toList
+encodeEvent' :: [Event] -> ByteString
+encodeEvent' = Cassava.encodeDefaultOrderedByName
 
-encodeEventsToFile :: FilePath -> Vector Event -> IO (Either String ())
+encodeEventsToFile :: FilePath -> [Event] -> IO (Either String ())
 encodeEventsToFile filePath = catchShowIO . BL.writeFile filePath . encodeEvent'
 
 -- Quando Either retornar String, significa que houve erro
@@ -171,18 +191,64 @@ catchShowIO action =
     handleIOException :: IOException -> IO (Either String a)
     handleIOException = return . Left . show
 
--- Funcao que adiciona um evento ao final de um arquivo csv. Se o arquivo não existir, ele é criado
+-- Função que, apos executar a leitra dos registro, transforma o Vector que é retornado em uma lista
+getEvents :: IO [Event]
+getEvents = do
+  Foldable.toList <$> ioVec
+  where
+    ioVec :: IO (Vector Event)
+    ioVec = do
+      file <- decodeEventsFromFile eventPath
+      return $ fromRight Vector.empty file
+
+-- Funcao que adiciona um evento ao final de um arquivo csv. Se o arquivo não existir, ele é criado. Primeiro preciso ler o arquivo que já existe, depois coloco e evento novo no final e escrevo tudo de volta no arquivo.
 insertToFile :: FilePath -> Event -> IO (Either String ())
-insertToFile filePath event = do
+insertToFile filePath newEvent = do
   eventVec <- file
-  let newVec = Vector.cons event eventVec
-  encodeEventsToFile filePath newVec
+  let eventList = Foldable.toList eventVec ++ [newEvent]
+  --let newVec = Vector.cons event eventVec
+  -- Não sei pq mas sem esse print o encode retorna que o arquivo ta bloqueado
+  print eventVec
+  encodeEventsToFile filePath eventList
   where
     -- Desenvolopo o vetor do Either
     file :: IO (Vector Event)
-    file = do 
+    file = do
         fileRead <- decodeEventsFromFile filePath
-        return $ fromRight (Vector.singleton event) fileRead
+        return $ fromRight (Vector.singleton newEvent) fileRead
+
+-- Recebe uma lista de eventos e transforma ela em um Vector de Eventos
+buildEventVector :: [Event] -> Vector Event
+buildEventVector = foldr Vector.cons Vector.empty
+
+-- Função que filtra uma lista de Eventos, selecionando aqueles que tem um valor especifico em um dos campos.
+-- Além da lista a ser filtrada e do valor deseja, é necessário passar qual dos campos de Event será comparado
+filterEvents :: Eq a => a -> (Event -> a) -> [Event] -> [Event]
+filterEvents value getter es = [e | e <- es, getter e == value]
+
+-- Recebe os eventos do csv e devolve apenas os de uma categoria especifica. category é a função definida em data Event
+filterEventsByCat :: Text -> [Event] -> [Event]
+filterEventsByCat catname = filterEvents catname category
+
+-- Filtra a lita de Eventos passada como parametro para devolver os eventos que acontecerão ANTES do dia informado
+filterCloseEvents :: Int -> Int -> Int -> [Event] -> [Event]
+filterCloseEvents d m y es = [e | e <- es, (year e < y) || (year e == y && month e < m) || (year e == y && month e == m && day e < d) ]
+
+-- Filtra a lita de Eventos passada como parametro para devolver os eventos que acontecerão DEPOIS do dia informado
+filterDistantEvets :: Int -> Int -> Int -> [Event] -> [Event]
+filterDistantEvets d m y es = [e | e <- es, (year e > y) || (year e == y && month e > m) || (year e == y && month e == m && day e > d) ]
+
+-- Filtra a lita de Eventos passada como parametro e devole aqueles que cuja substring de argumento está presente no nome do evento
+filterEventName :: Text -> [Event] -> [Event]
+filterEventName n es = [e | e <- es, n `isInfixOf` name e]
+
+-- Filtra a lita de Eventos passada como parametro e devolve apenas eventos que tem alguma descrição
+filterHasDesc :: [Event] -> [Event]
+filterHasDesc es = [e | e <- es, description e /= ""]
+
+-- Filtra a lita de Eventos passada como parametro e devolve apenas eventos que tem são recorrentes
+filterIsReg ::[Event] -> [Event]
+filterIsReg es = [e | e <- es, recurrent e]
 
 testrecord :: ByteString
 testrecord = "name,day,month,year,description,category,recurrent,color\namanha,01,01,2022,insert description,none,False,Gradient\n"
@@ -228,9 +294,9 @@ main = do
   let cod1 = encodeEvent [eventrecord, eventrecord]
 
   let vec = Vector.singleton eventrecord
-  let cod2 = encodeEvent' vec
+  let cod2 = encodeEvent' $ Foldable.toList vec
 
-  _ <- encodeEventsToFile "./csv/codificado.csv" vec
+  _ <- encodeEventsToFile "./csv/codificado.csv" $ Foldable.toList vec
 
   registro5 <- insertToFile "./csv/codificadoooo.csv" eventrecord
 
